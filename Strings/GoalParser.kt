bed9417f0f403d4a7c40ce382fcc87dceda29ee5
SSdCode <dubeysamir514@gmail.com>
2023-10-05 23:38:19 +0530

    comment

--- Strings/GoalParser.kt
+++ Strings/GoalParser.kt
@@ -1,3 +1,11 @@
+// Input: command = "G()(al)"
+// Output: "Goal"
+// Explanation: The Goal Parser interprets the command as follows:
+// G -> G
+// () -> o
+// (al) -> al
+// The final concatenated result is "Goal".
+
 fun main() {
     println(interpret("G()(al)"))
 }
